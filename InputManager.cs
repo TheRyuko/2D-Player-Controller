ecb98dd2288d2994bc114f777e9653c6fa34ba48
TheRyuko <58053309+TheRyuko@users.noreply.github.com>
2019-11-21 16:32:40 -0600

    Add files via upload

---
+++ InputManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [Range(0, 10)]
+    public int axisCount;
+    [Range(0, 20)]
+    public int buttonCount;
+
+    public Controller controller;
+
+    public void PassInput(InputData data)
+    {
+       
+        //Debug.Log ("Movement:" + data.axis[0]);
+        controller.ReadInput(data);
+
+    }
+
+    public void RefreshTracker()
+    {
+        DeviceTracker dt = GetComponent<DeviceTracker>();
+        if (dt != null)
+        {
+            dt.Refresh();
+        }
+    }
+    
+}
+
+public struct InputData
+{
+    public float[] axis;
+    public bool[] buttons;
+
+    public InputData(int axesCount, int buttonCount)
+    {
+        axis = new float[axesCount];
+        buttons = new bool[buttonCount];
+    }
+    
+    public void Reset()
+    {
+        for (int i = 0; i < axis.Length; i++)
+        {
+            axis[i] = 0f;
+        }
+
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i] = false;
+        }
+    }
+}
---
+++ KeyboardTracker.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardTracker : DeviceTracker
+{
+    public AxisKeys[] axisKeys;
+    public KeyCode[] buttonKeys;
+
+    void Reset()
+    {
+        im = GetComponent<InputManager>();
+        axisKeys = new AxisKeys[im.axisCount];
+        buttonKeys = new KeyCode[im.buttonCount];
+    }
+
+    public override void Refresh()
+    {
+        im = GetComponent<InputManager>();
+
+        // 2 temp arrays for buttons and axis
+        KeyCode[] newButtons = new KeyCode[im.buttonCount];
+        AxisKeys[] newAxis = new AxisKeys[im.axisCount];
+
+        if(buttonKeys != null)
+        {
+            for (int i = 0; i < Mathf.Min(newButtons.Length, buttonKeys.Length); i++)
+            {
+                newButtons[i] = buttonKeys[i];
+            }
+        }
+        buttonKeys = newButtons;
+
+        if(axisKeys != null)
+        {
+            for (int i = 0; i < Mathf.Min(newAxis.Length, axisKeys.Length); i++)
+            {
+                newAxis[i] = axisKeys[i];
+            }
+        }
+        axisKeys = newAxis;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check for input. if new input detected set true clear
+        //Populate InputData to pass to the Input Manager
+
+        for (int i = 0; i < axisKeys.Length; i++)
+        {
+            float val = 0f;
+            if (Input.GetKey (axisKeys[i].positive))
+            {
+                val += 1f;
+                newData = true;
+            }
+
+            if (Input.GetKey (axisKeys[i].negative))
+            {
+                val -= 1f;
+                newData = true;
+            }
+
+            data.axis[i] = val;
+
+        }
+    
+        for (int i = 0; i < buttonKeys.Length; i++)
+        {
+            if (Input.GetKey(buttonKeys[i]))
+            {
+                data.buttons[i] = true;
+                newData = true;
+            }
+        }
+        
+        if (newData)
+        {
+            im.PassInput(data);
+            newData = false;
+            data.Reset();
+        }
+    }
+}
+
+[System.Serializable]
+public struct AxisKeys
+{
+    public KeyCode positive;
+    public KeyCode negative;
+}
